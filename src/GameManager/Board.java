d0d0bb5f5cea70a63a86c541c1c70efef4ac21a5
schaefflerf <florian.schaeffler1@hs-augsburg.de>
2022-03-22 13:12:39 +0100

    Board: added boardfiller

--- src/GameManager/Board.java
+++ src/GameManager/Board.java
@@ -1,4 +1,22 @@
 package GameManager;
 
+import java.util.ArrayList;
+
 public class Board {
+    private int rows = 6;
+    private int cols = 8;
+    private char[][] board = new char[rows][cols];
+    private String currentPlayer;
+    private ArrayList<String> playerBase = new ArrayList<>();
+
+    //board-Array mit "." füllen
+    public void fillBoard(){
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j <  cols; j++){
+
+                board[i][j] = '.';
+            }
+        }
+    }
 }
